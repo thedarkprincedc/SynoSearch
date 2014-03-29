718c85348bac02bbe7a8992b6fac5626dd147b44
Brett <brettmosley@hotmail.com>
2014-03-29 12:43:58 -0400

    Added Configuration.ini code and tested

--- search/synosearch.php
+++ search/synosearch.php
@@ -1,30 +1,34 @@
 <?php
-	$ini_array = parse_ini_file("configuration.ini", true);
+	
+	$ini_array = parse_ini_file("Configuration.ini", true);
+	$search_configs;
+	foreach ($ini_array as $key => $value) {
+		
+		
+		if( preg_match("/[A-Za-z0-9]*_configuration/", $key) == 1){
+			$arry = explode("/", $ini_array[$key]["full_path"]);
+			$ini_array[$key]["drive"] = $arry[1];
+			$rw = array_slice($arry,2,(count($arry)-3));
+			$ini_array[$key]["path"] = implode("/", $rw);
+			$search_configs[$key]=$ini_array[$key];
+		}
+		
+	}
 	$query_type = (isset($_GET["query"]) != "") ? strtolower ($_GET["query"]) : "";
 	$search_term = (isset($_GET["search"]) != "") ? $_GET["search"] : "";
 	
 	$url_file = "";
 	$base_url	= "";
-	switch($query_type){
-		case "books":
-			$url_file = "/volume1/shared/books/bookindex.txt";
-			$base_url = "shared/books";
-		break;
-		case "videos":
-			$url_file = "/volume1/video/videoindex.txt";
-			$base_url = "video";
-		break;
-		case "photos":
-			$url_file = "/volume1/photo/photoindex.txt";
-			$base_url = "photo";
-		break;
-		default:
-			$url_file = "/volume1/shared/books/bookindex.txt";
-			$base_url = "shared/books";
-	}
-	//echo $query_type . " " . $search_term ;
-	
+	foreach($search_configs as $key => $value){
+		$name = $search_configs[$key]["name"];
+		if(strcasecmp($name, $query_type) == 0){
+			$url_file = $search_configs[$key]["full_path"];
+			$base_url = $search_configs[$key]["path"];
+		}
+	} 
+
 	$handle = @fopen($url_file, "r");
+
 	if ($handle) {
 		$trs = array();
 		while (($buffer = fgets($handle, 4096)) !== false) {
